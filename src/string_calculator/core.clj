485963c7b64a276f304b9e05f53a75afc8f73ccd
Nicolas Chapon <nchapon@gmail.com>
2013-10-03 09:48:39 +0200

    When multiple negatives show all of them in exception message

--- src/string_calculator/core.clj
+++ src/string_calculator/core.clj
@@ -16,13 +16,13 @@
      (not (empty? s-terms)) s-terms
      :else (vector terms "[,\n]"))))
 
-
 (defn parse-numbers
   [numbers sep]
   (let [numbers (map #(Integer/parseInt %) (.split numbers sep))]
-    (if-let [negatives (filter neg? numbers)]
+    (if-let [negatives (seq (filter neg? numbers))] ;; filter does not returns nil
       (throw (IllegalArgumentException.
-              (str "Negatives not allowed " (apply str negatives))))
+              (str "Negatives not allowed " (apply str
+                                                   (interpose " " negatives)))))
       numbers)))
 
 (defn add
@@ -42,6 +42,7 @@
 (fact "Add two numbers"
   (add "1,2") => 3)
 
+;.;. Excellence is not an act but a habit. -- Aristotle
 (fact "Add an unknow amount of numbers"
   (add "1,2,3") => 6)
 
@@ -57,10 +58,12 @@
 (fact "When no line separator"
   (parse-terms "1,2,3") => ["1,2,3" "[,\n]"])
 
-
 (fact "Support different delimiters"
   (add "//;\n1;2;3") => 6)
 
-
-(fact "Callind add with a negative number will throw an exception 'negatives not allowed'"
-  (add "-1,2") => (throws IllegalArgumentException "Negatives not allowed -1"))
+(fact
+  "Callind add with a negative number will throw an exception
+   'negatives not allowed' - and the negative that was passed.
+    If there are multiple negatives, show all of them in the exception message."
+  (add "-1,2") => (throws IllegalArgumentException "Negatives not allowed -1")
+  (add "-1,-2,3,-4,5") => (throws IllegalArgumentException "Negatives not allowed -1 -2 -4"))
